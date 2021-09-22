0eea8105467b55b9afd9772adb15a988f817baee
Rafael Delgado Cubilla <rdelcub359@g.educaand.es>
2021-09-22 10:16:54 +0200

    V1.2

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("¡Hola mundo!");
 		System.out.println("¡Hola mundo AMPLIADO!");
+		System.out.println("!V1.2");
 	}
 }
